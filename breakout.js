5ec0f3509adf1f27181c9a357be6101ca8687c4b
Karla Šmuk <karla.smuk@gmail.com>
2024-11-11 22:13:05 +0100

    initialize bricks and draw them

--- breakout.js
+++ breakout.js
@@ -7,14 +7,63 @@ let ballX,
 const velocityX = 2;
 const velocityY = -velocityX;
 
-//brickets parameters
-const brickRow = 8;
-const brickColumn = 14;
+//initialize bricks
+const brickRows = 8;
+const brickColumns = 14;
+let bricks = new Array();
+for (let i = 0; i < brickRows; i++) {
+  //row array
+  bricks[i] = new Array();
+  for (let j = 0; j < brickColumns; j++) {
+    //column in row
+    bricks[i].push({ draw: true });
+  }
+}
 
 //points
 let points = 0;
 //highest score save to local storage
 let highestScore = localStorage.getItem("highestScore");
+
+function getColor(index) {
+  switch (index) {
+    case 2:
+      return "#fb8500";
+    case 3:
+      return "#fb8500";
+    case 4:
+      return "#80b918";
+    case 5:
+      return "#80b918";
+    case 6:
+      return "#ffea00";
+    case 7:
+      return "#ffea00";
+    default:
+      return "#c1121f";
+  }
+}
+
+function drawBricks() {
+  for (let i = 0; i < brickRows; i++) {
+    for (let j = 0; j < brickColumns; j++) {
+      if (bricks[i][j].draw == true) {
+        //column width
+        const brickX = j * (canvas.width / brickColumns);
+        //row height
+        const brickY = i * 20;
+
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, canvas.width / brickColumns, 20);
+        ctx.shadowBlur = 20;
+        ctx.shadowColor = "black";
+        ctx.fillStyle = getColor(i);
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
+}
 
 function drawBall() {
   ctx.beginPath();
@@ -25,7 +74,9 @@ function drawBall() {
 }
 
 function startGame() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawBricks();
 }
 
 function resizeCanvas() {
